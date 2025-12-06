badca9bf88da9b13cd7e5ad416d34a2432c10936
gafard <gnane.gafard@gmail.com>
2025-12-06 13:05:00 +0000

    auto: sync 2025-12-06T13:05:00

---
+++ js/articles-data.js
@@ -0,0 +1,11 @@
+// Données des articles intégrées directement dans le JavaScript
+// Cela permet au CMS de fonctionner même sans serveur local (file://)
+// Généré automatiquement depuis data/articles.json
+
+const ARTICLES_DATA = {
+  "apologetique": [],
+  "science": [],
+  "histoire": [],
+  "archeologie": []
+};
+
